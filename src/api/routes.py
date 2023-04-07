71fbd2523acb81119f2c6e952f117df752a7d233
Andrew <andrewguan23@gmail.com>
2023-04-06 20:20:32 -0700

    make steam request

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from api.steamHours import steamHours
 
 api = Blueprint('api', __name__)
 
@@ -13,6 +14,18 @@ def handle_hello():
 
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/getSteamHours', methods=['POST', 'GET'])
+def getSteamHours():
+
+    id = request.args.get("id")
+
+    response_body = {
+
+        "message": steamHours(id)
     }
 
     return jsonify(response_body), 200
--- src/api/steamHours.py
+++ src/api/steamHours.py
@@ -3,6 +3,8 @@ from decouple import config
 import math
 import json
 
+
+
 #digit is # after .
 def truncate(number, digits) -> float:
     nbDecimals = len(str(number).split('.')[1]) 
@@ -15,17 +17,26 @@ def getHeaderImage(appid) -> str:
     appDataJson = json.loads(steam.apps.get_app_details(appid))
     return appDataJson[str(appid)]["data"]["header_image"]
 
-KEY = config("STEAM_API_KEY")
+def steamHours(steamid) -> str:
+    KEY = config("STEAM_API_KEY")
+    steam = Steam(KEY)
+    # arguments: steamid
+    # my steam id: 76561198431671719
+    user = steam.users.get_owned_games(steamid)
+    games = user["games"]
 
-steam = Steam(KEY)
+    result = ""
 
-# arguments: steamid
-user = steam.users.get_owned_games("76561198431671719")
+    for attribute in games:
+         result += (getHeaderImage(attribute["appid"]) + "  "  + attribute["name"] 
+            + "  " + str(truncate(attribute["playtime_forever"]/60.0, 1)) + " hours\n")
+         
+    return result
 
-games = user["games"]
 
-for attribute in games:
-    print(getHeaderImage(attribute["appid"]) + "  "  + attribute["name"] 
-          + "  " + str(truncate(attribute["playtime_forever"]/60.0, 1)) + " hours")
+
+
+
+
     
 
