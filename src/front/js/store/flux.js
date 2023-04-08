6e4bfe3928b1a0a8b46312495e356a91ec6768ac
Andrew <andrewguan23@gmail.com>
2023-04-07 19:44:05 -0700

    display api/hello message on frontend

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -24,9 +24,11 @@ const getState = ({ getStore, getActions, setStore }) => {
 			getMessage: async () => {
 				try{
 					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
+					// console.log("process.env.BACKEND_URL", process.env.BACKEND_URL)
+					// const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
+					const resp = await fetch("http://localhost:3001/api/hello")
 					const data = await resp.json()
-					setStore({ message: data.message })
+					setStore({ message: data.message})
 					// don't forget to return something, that is how the async resolves
 					return data;
 				}catch(error){
